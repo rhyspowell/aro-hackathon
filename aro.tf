8a749fe9862fb7d38455c6fe72736d58cd248e1a
George <ge.pagiatakis@gmail.com>
2024-07-10 12:01:12 +0100

    aro tf file

--- aro.tf
+++ aro.tf
@@ -1,126 +1,140 @@
-
-#enforce specific version
-
 terraform {
+  required_version = ">= 1.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.111.0"
+      version = ">=3.3.0"
+    }
+    azapi = {
+      source  = "Azure/azapi"
+      version = ">=1.0.0"
     }
   }
 }
 
-
-
-
-# SP details and where to deploy etc
-
 provider "azurerm" {
   features {}
-  client_id       = "YOUR_CLIENT_ID"
-  client_secret   = "YOUR_CLIENT_SECRET"
-  tenant_id       = "YOUR_TENANT_ID"
-  subscription_id = "YOUR_SUBSCRIPTION_ID"
 }
 
-
-
-resource "azurerm_resource_group" "db_hackathon" {
-  name     = "db-hackathon"
-  location = "East US"
+provider "azapi" {
 }
 
-resource "azurerm_virtual_network" "aro_vnet" {
-  name                = "aro-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.db_hackathon.location
-  resource_group_name = azurerm_resource_group.db_hackathon.name
+data "azurerm_client_config" "current" {
+}
+
+locals {
+  resource_group_id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/aro-${var.domain}-${var.location}"
+}
+
+resource "random_string" "resource_prefix" {
+  length  = 6
+  special = false
+  upper   = false
+  numeric  = false
+}
+
+resource "azurerm_virtual_network" "virtual_network" {
+  name                = "${var.resource_prefix != "" ? var.resource_prefix : random_string.resource_prefix.result}VNet"
+  address_space       = var.virtual_network_address_space
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  tags                = var.tags
+  
+  lifecycle {
+    ignore_changes = [
+        tags
+    ]
+  }
 }
 
 resource "azurerm_subnet" "master_subnet" {
-  name                 = "master-subnet"
-  resource_group_name  = azurerm_resource_group.db_hackathon.name
-  virtual_network_name = azurerm_virtual_network.aro_vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-  service_endpoints    = ["Microsoft.ContainerRegistry"]
+  name                                           = var.master_subnet_name
+  resource_group_name                            = var.resource_group_name
+  virtual_network_name                           = azurerm_virtual_network.virtual_network.name
+  address_prefixes                               = var.master_subnet_address_space
+  private_link_service_network_policies_enabled  = false
+  service_endpoints                              = ["Microsoft.ContainerRegistry"]
 }
 
 resource "azurerm_subnet" "worker_subnet" {
-  name                 = "worker-subnet"
-  resource_group_name  = azurerm_resource_group.db_hackathon.name
-  virtual_network_name = azurerm_virtual_network.aro_vnet.name
-  address_prefixes     = ["10.0.2.0/24"]
-  service_endpoints    = ["Microsoft.ContainerRegistry"]
+  name                                           = var.worker_subnet_name
+  resource_group_name                            = var.resource_group_name
+  virtual_network_name                           = azurerm_virtual_network.virtual_network.name
+  address_prefixes                               = var.worker_subnet_address_space
+  service_endpoints                              = ["Microsoft.ContainerRegistry"]
 }
 
-resource "azurerm_network_security_group" "example" {
-  name                = "example-nsg"
-  location            = azurerm_resource_group.db_hackathon.location
-  resource_group_name = azurerm_resource_group.db_hackathon.name
-}
-
-resource "azurerm_subnet_network_security_group_association" "master" {
-  subnet_id                 = azurerm_subnet.master_subnet.id
-  network_security_group_id = azurerm_network_security_group.example.id
-}
-
-resource "azurerm_subnet_network_security_group_association" "worker" {
-  subnet_id                 = azurerm_subnet.worker_subnet.id
-  network_security_group_id = azurerm_network_security_group.example.id
-}
-
-
-# if needed 
-resource "azurerm_network_security_rule" "example" {
-  name                        = "example-rule"
-  priority                    = 100
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "*"
-  source_port_range           = "*"
-  destination_port_range      = "*"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  network_security_group_name = azurerm_network_security_group.example.name
-}
-
-resource "azurerm_role_assignment" "example" {
-  scope                = azurerm_resource_group.db_hackathon.id
+resource "azurerm_role_assignment" "aro_cluster_service_principal_network_contributor" {
+  scope                = azurerm_virtual_network.virtual_network.id
   role_definition_name = "Contributor"
-  principal_id         = "XXXXXX" # Service principal ID
+  principal_id         = var.aro_cluster_aad_sp_object_id
+  skip_service_principal_aad_check = true
 }
 
-resource "azurerm_openshift_cluster" "example" {
-  name                = "example-openshift-cluster"
-  location            = azurerm_resource_group.db_hackathon.location
-  resource_group_name = azurerm_resource_group.db_hackathon.name
-
-  cluster_profile {
-    pull_secret      = "XXXXXX" # usual RH pull secret
-    domain           = "example.com"
-    resource_group_id = azurerm_resource_group.db_hackathon.id
-  }
-
-  service_principal {
-    client_id     = "XXXXXX"
-    client_secret = "XXXXXX"
-  }
-
-  network_profile {
-    pod_cidr      = "10.128.0.0/14"
-    service_cidr  = "172.30.0.0/16"
-  }
-
-  master_profile {
-    vm_size     = "Standard_D8s_v3"
-    subnet_id   = azurerm_subnet.master_subnet.id
-  }
-
-  worker_profile {
-    name       = "worker"
-    vm_size    = "Standard_D4s_v3"
-    subnet_id  = azurerm_subnet.worker_subnet.id
-    count      = 3
-  }
+resource "azurerm_role_assignment" "aro_resource_provider_service_principal_network_contributor" {
+  scope                = azurerm_virtual_network.virtual_network.id
+  role_definition_name = "Contributor"
+  principal_id         = var.aro_rp_aad_sp_object_id
+  skip_service_principal_aad_check = true
 }
 
+data "azurerm_resource_group" "resource_group" {
+  name                = var.resource_group_name
+}
+
+resource "azapi_resource" "aro_cluster" {
+  name      = "${var.resource_prefix != "" ? var.resource_prefix : random_string.resource_prefix.result}Aro"
+  location  = var.location
+  parent_id = data.azurerm_resource_group.resource_group.id
+  type      = "Microsoft.RedHatOpenShift/openShiftClusters@2022-04-01"
+  tags      = var.tags
+  
+  body = jsonencode({
+    properties = {
+      clusterProfile = {
+        domain               = var.domain
+        fipsValidatedModules = var.fips_validated_modules
+        resourceGroupId      = local.resource_group_id
+        pullSecret           = var.pull_secret
+      }
+      networkProfile = {
+        podCidr              = var.pod_cidr
+        serviceCidr          = var.service_cidr
+      }
+      servicePrincipalProfile = {
+        clientId             = var.aro_cluster_aad_sp_client_id
+        clientSecret         = var.aro_cluster_aad_sp_client_secret
+      }
+      masterProfile = {
+        vmSize               = var.master_node_vm_size
+        subnetId             = azurerm_subnet.master_subnet.id
+        encryptionAtHost     = var.master_encryption_at_host
+      }
+      workerProfiles = [
+        {
+          name               = var.worker_profile_name
+          vmSize             = var.worker_node_vm_size
+          diskSizeGB         = var.worker_node_vm_disk_size
+          subnetId           = azurerm_subnet.worker_subnet.id
+          count              = var.worker_node_count
+          encryptionAtHost   = var.worker_encryption_at_host
+        }
+      ]
+      apiserverProfile = {
+        visibility           = var.api_server_visibility
+      }
+      ingressProfiles = [
+        {
+          name               = var.ingress_profile_name
+          visibility         = var.ingress_visibility
+        }
+      ]
+    }
+  })
+
+  lifecycle {
+    ignore_changes = [
+        tags
+    ]
+  }
+}
