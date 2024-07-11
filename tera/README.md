c60469b0515c9e8802712599b6c06f214aa4d080
Rhys <rhys@redhat.com>
2024-07-11 11:06:08 +0100

    backend variable fix. Add readme

---
+++ tera/README.md
@@ -0,0 +1,9 @@
+# Running Instructions
+
+To allow for the storage of the terraform state a storage resource group, account name and container name need to be created manually.
+Once created export the values
+
+export STORAGE_ACCOUNT_RESOURCE_GROUP=xxxxxxx
+export STORAGE_ACCOUNT_NAME=xxxxxxx
+export STORAGE_CONTAINER_NAME=tfstate
+export KEY=xxxxxx
--- tera/terraform.tf
+++ tera/terraform.tf
@@ -11,10 +11,10 @@ terraform {
 
   // Backend state is recommended but not required, this block can be removed for testing environments. To use it , set up a storage account on Azure , create a container and reference the details on the block below
   backend "azurerm" {
-    resource_group_name  = var.storage_resource_group_name
-    storage_account_name = var.storage_account_name
-    container_name       = var.container_name
-    key                  = var.key
+    resource_group_name  = "$STORAGE_RESOURCE_GROUP_NAME"
+    storage_account_name = "$STORAGE_ACCOUNT_NAME"
+    container_name       = "$CONTAINER_NAME"
+    key                  = "$KEY"
   }
 }
 
